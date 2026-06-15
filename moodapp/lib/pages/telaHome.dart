6f6447d89294dce71ef6d5c35c3411726c7fe332
trvzera <giovanni.cm.trivellato@gmail.com>
2026-06-15 08:21:58 -0300

    Ajuste nas animações e criação de dois novos botões: Tranquilo e Triste. Além de ajuste no SetState para o texto do app

--- moodapp/lib/pages/telaHome.dart
+++ moodapp/lib/pages/telaHome.dart
@@ -9,6 +9,7 @@ class _TelaHomeState extends State<TelaHome> {
   bool feliz = false;
   bool tranquilo = false;
   bool triste = false;
+  String mensagem = "Escolha o humor que melhor define o dia de hoje!";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +30,7 @@ class _TelaHomeState extends State<TelaHome> {
           padding: EdgeInsets.all(16),
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             Text(
-              "Escolha o humor que melhor define o dia de hoje!",
+              mensagem,
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 14,
@@ -38,24 +39,107 @@ class _TelaHomeState extends State<TelaHome> {
               ),
             ),
             const SizedBox(height: 20),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    feliz = !feliz;
-                  });
-                },
-                child: AnimatedContainer(
-                  duration: const Duration(seconds: 1),
-                  width: feliz ? 120 : 60,
-                  height: feliz ? 120 : 60,
-                  decoration: BoxDecoration(
-                      color: feliz ? Colors.yellow : Colors.orange,
-                      borderRadius: BorderRadius.circular(feliz ? 100 : 10)),
-                  child: Icon(Icons.face, color: Colors.white),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      feliz = !feliz;
+                      if (triste) {
+                        triste = false;
+                      }
+                      if (tranquilo) {
+                        tranquilo = false;
+                      }
+                      if (feliz) {
+                        mensagem = "Hoje estou feliz!";
+                      } else {
+                        mensagem =
+                            "Escolha o humor que melhor define o dia de hoje!";
+                      }
+                    });
+                  },
+                  child: AnimatedContainer(
+                    duration: const Duration(seconds: 1),
+                    width: feliz ? 75 : 60,
+                    height: feliz ? 75 : 60,
+                    decoration: BoxDecoration(
+                        color: feliz ? Colors.orangeAccent : Colors.orange,
+                        borderRadius: BorderRadius.circular(feliz ? 100 : 10)),
+                    child: Icon(Icons.face,
+                        color: const Color.fromARGB(255, 255, 241, 178)),
+                  ),
                 ),
-              ),
-            ])
+                const SizedBox(width: 20),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      tranquilo = !tranquilo;
+                      if (triste) {
+                        triste = false;
+                      }
+                      if (feliz) {
+                        feliz = false;
+                      }
+                      if (tranquilo) {
+                        mensagem = "Hoje estou tranquilo!";
+                      } else {
+                        mensagem =
+                            "Escolha o humor que melhor define o dia de hoje!";
+                      }
+                    });
+                  },
+                  child: AnimatedOpacity(
+                    opacity: tranquilo ? 0.5 : 1,
+                    duration: const Duration(seconds: 1),
+                    child: AnimatedContainer(
+                        duration: const Duration(seconds: 1),
+                        width: tranquilo ? 90 : 60,
+                        height: tranquilo ? 50 : 60,
+                        decoration: BoxDecoration(
+                          color: tranquilo
+                              ? Colors.lightBlueAccent
+                              : Colors.lightBlue,
+                          borderRadius:
+                              BorderRadius.circular(tranquilo ? 30 : 10),
+                        ),
+                        child: Icon(Icons.cloud, color: Colors.white)),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      triste = !triste;
+                      if (feliz) {
+                        feliz = false;
+                      }
+                      if (tranquilo) {
+                        tranquilo = false;
+                      }
+                      if (triste) {
+                        mensagem = "Hoje estou triste!";
+                      } else {
+                        mensagem =
+                            "Escolha o humor que melhor define o dia de hoje!";
+                      }
+                    });
+                  },
+                  child: AnimatedContainer(
+                    duration: const Duration(seconds: 1),
+                    width: triste ? 50 : 60,
+                    height: triste ? 50 : 60,
+                    decoration: BoxDecoration(
+                        color: triste ? Colors.blue : Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(triste ? 0 : 10)),
+                    child: Icon(Icons.water,
+                        color: const Color.fromARGB(255, 177, 211, 255)),
+                  ),
+                ),
+              ]),
+            ),
           ]),
         ),
       ),
